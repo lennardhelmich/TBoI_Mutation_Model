46dac3620724e1fc3937ad304f7fdeaae304d94d
Charescos-PC\Lennard Arbeit <lennard.helmich@gmx.de>
2025-02-18 14:41:55 +0100

    Finalized first version of xml_to_bitmap_converter

--- XMLToBitmapConverter/xml_to_bitmap_converter.py
+++ XMLToBitmapConverter/xml_to_bitmap_converter.py
@@ -4,7 +4,7 @@ import os
 from PIL import Image
 from enum import Enum
 
-class BitmapValues(Enum):
+class EntityType(Enum):
     WALL = 0
     DOOR = 1
     FREE_SPACE = 2
@@ -18,86 +18,80 @@ class BitmapValues(Enum):
     POOP = 10
     SPIKE = 11
 
+# Function to evenly assign pixel values with count of different entities
 def get_pixel_value_with_entity_id(entity_id):
-    entity_count = len(BitmapValues) - 1
-    print(f"Entity Count : {entity_count} \n")
+    entity_count = len(EntityType) - 1
     steps = 255 / entity_count
-    print(f"Bit Value Steps : {steps} \n")
     return math.floor(entity_id.value*steps)
 
+# Function to parse the correct simplified EntityType out of TBoI entity_types
 def handle_entity_values(value):
     if value == 0:
-        return BitmapValues.FREE_SPACE
+        return EntityType.FREE_SPACE
     elif value == 5:
-        return BitmapValues.PICKUP
+        return EntityType.PICKUP
     elif value == 3000:
-        return BitmapValues.PIT
+        return EntityType.PIT
     elif value == 33:
-        return BitmapValues.FIRE
+        return EntityType.FIRE
     elif value == 6:
-        return BitmapValues.MACHINE
+        return EntityType.MACHINE
     elif value == 1900:
-        return BitmapValues.BLOCK
+        return EntityType.BLOCK
     elif 10 <= value <= 900:
-        return BitmapValues.ENTITY
+        return EntityType.ENTITY
     elif 1000 <= value <= 1100:
-        return BitmapValues.STONE
+        return EntityType.STONE
     elif 1490 <= value <= 1510:
-        return BitmapValues.POOP
+        return EntityType.POOP
     elif 1930 <= value <= 2000:
-        return BitmapValues.SPIKE
+        return EntityType.SPIKE
     else:
         print("The following entity_type is not defined in BitmapValues : {value}")
 
-def read_xml_file(file_path):
+#Function to convert a xml with Room Layout Data from basement_renovator into a 8bit bitmap 
+def convert_xml_to_bitmap(file_path):
     try:
         tree = ET.parse(file_path)
         root = tree.getroot()
         
-        # Print the root element
-        print(f"Root element: {root.tag}")
         index = 0
+        #Iterate through all defined Rooms from XML
         for room in root.findall('room'):
-            print(f"\nRoom Attributes:")
-            for attr in room.attrib:
-                print(f"  {attr}: {room.attrib[attr]}")
 
             #create Bitmap
             width, height = 15,9
             bitmap_image = Image.new('L', (width, height), 0)
 
+            #Initialize the bitmap of the room with free Spaces
             room_width, room_height = 13,7
             for width_index in range(room_width):
                 for height_index in range(room_height):
-                    bitmap_image.putpixel((width_index+1,height_index+1),get_pixel_value_with_entity_id(BitmapValues.FREE_SPACE))
+                    bitmap_image.putpixel((width_index+1,height_index+1),get_pixel_value_with_entity_id(EntityType.FREE_SPACE))
 
-            # Handle doors
-            print("\nDoors:")
+            # Handle existing doors
             for door in room.findall('door'):
                 exists = door.get('exists')
                 x = int(door.get('x')) + 1
                 y = int(door.get('y')) + 1
                 if(exists):
-                    bitmap_image.putpixel((x,y),get_pixel_value_with_entity_id(BitmapValues.DOOR))
-                print(f"  Door exists: {exists}, Position: ({x}, {y})")
+                    bitmap_image.putpixel((x,y),get_pixel_value_with_entity_id(EntityType.DOOR))
 
-            # Handle spawns
-            print("\nSpawns:")
+            # Handle every single entity
             for spawn in room.findall('spawn'):
                 x = int(spawn.get('x')) + 1
                 y = int(spawn.get('y')) + 1
                 entity = spawn.find('entity')
                 if entity is not None:
                     entity_type = int(entity.get('type'))
-                    variant = entity.get('variant')
-                    subtype = entity.get('subtype')
-                    weight = entity.get('weight')
                     bitmap_image.putpixel((x,y),get_pixel_value_with_entity_id(handle_entity_values(entity_type)))
-                    print(f"  Spawn Position: ({x}, {y}), Entity Type: {entity_type}, Variant: {variant}, Subtype: {subtype}, Weight: {weight}")
 
+            # Save bitmap of current room 
             directory = "Bitmaps"
             file_path = os.path.join(directory, f"bitmap_{index}.bmp")
             bitmap_image.save(file_path)
+
+            # Increase index for bitmap count
             index += 1
 
     except ET.ParseError as e:
@@ -109,7 +103,5 @@ def read_xml_file(file_path):
 
 # Example usage
 if __name__ == "__main__":
-    xml_file_path = 'Rooms/SecondInputRooms.xml'  # Replace with your XML file path
-    read_xml_file(xml_file_path)
-    # print(get_pixel_value_with_entity_id(BitmapValues.WALL))
-    # print(get_pixel_value_with_entity_id(BitmapValues.SPIKE))
+    xml_file_path = 'Rooms/SecondInputRooms.xml'
+    convert_xml_to_bitmap(xml_file_path)
